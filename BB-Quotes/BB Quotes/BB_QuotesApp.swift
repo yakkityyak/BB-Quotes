61c6cf102e3a587bfe310d7814c391a748e4dfb0
Jakob <jakob.hjortshoj@noaignite.com>
2022-08-30 09:30:03 +0200

    Project setup done

---
+++ BB-Quotes/BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by Jakob Hjortshøj on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BB-Quotes/BB Quotes/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  BB Quotes
+//
+//  Created by Jakob Hjortshøj on 30/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView{
+            Text("Breaking Bad View")
+                .tabItem {
+                    Label("Breaking Bad", systemImage: "tortoise")
+                }
+            
+            Text("Better Call Saul View")
+                .tabItem {
+                    Label("Better Call Saul", systemImage: "briefcase")
+                }
+        }
+        .onAppear{
+            UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
